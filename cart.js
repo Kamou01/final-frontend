ed4833abf7247bfff2fd02fa2b5f6be189a4fb25
Kamogelo Mkonto <82159075+Kamou01@users.noreply.github.com>
2021-09-23 04:41:00 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -14,9 +14,8 @@ function renderCart(cart) {
       <h3 class="card-text">${cart.description}</h3>
       <h4>${cart.price}</h4>
    </div>
-  </div>
+  </div>`
 }
-
 console.log(cart)
 
 function removeFromCart(id) {
